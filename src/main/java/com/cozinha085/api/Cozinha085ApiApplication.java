81e54b5e34f1bbf061ff4c4084d940436655d9e2
Aryellison Pontes <aryellison.dev@gmail.com>
2026-06-08 10:21:29 -0300

    chore: setup inicial do projeto e configuracao de dependencias

---
+++ src/main/java/com/cozinha085/api/Cozinha085ApiApplication.java
@@ -0,0 +1,13 @@
+package com.cozinha085.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cozinha085ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cozinha085ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cozinha085/api/Cozinha085ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozinha085.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cozinha085ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
